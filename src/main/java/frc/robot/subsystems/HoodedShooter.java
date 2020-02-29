19d25614bdd0b2e65eb7e5727ba2d335df1f0084
FRC 3958 Controls <robotics3958@students.bchigh.edu>
2020-02-29 15:02:29 -0500

    cleaning up and limelight on and off in auto align (maybe)

--- src/main/java/frc/robot/subsystems/HoodedShooter.java
+++ src/main/java/frc/robot/subsystems/HoodedShooter.java
@@ -10,13 +10,10 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.InvertType;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.constants.Constants;
 import frc.robot.constants.ShooterConstants;
 
 public class HoodedShooter extends SubsystemBase {
